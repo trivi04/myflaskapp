599e96c508cbf491bbe54dd3491efed4fd282df9
flask crud <trivikramkrovi.04@gmail.com>
2024-09-16 17:45:09 +0530

    before impementung edit and delete

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-from flask import Flask,render_template
-# from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 #myapp
-app=Flask(__name__)
+app = Flask(__name__)
 
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
 
+class MyTask(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(100),nullable=False)
+    Complete=db.Column(db.Integer,default=0)
+    created=db.Column(db.DateTime,default=datetime.utcnow)
 
-@app.route("/")
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+#home page
+@app.route("/",methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+    # add a task
+    if request.method=="POST":
+        current_task=request.form['content']
+        new_task=MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
 
-if __name__ in "__main__":
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
+    # see all tasks
+
+    else:
+        tasks=MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html',tasks=tasks)
+
+if __name__ == "__main__":
+
+
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+ 
